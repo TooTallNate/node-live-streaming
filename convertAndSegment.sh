f7a11497d06fc16ed46d146ddd8ef38f84ad3d57
Nathan Rajlich <nathan@tootallnate.net>
2011-01-25 20:14:00 -0800

    Use the correct 'segmenter'. i.e. the submodule one

--- convertAndSegment.sh
+++ convertAndSegment.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
+LOC=$(dirname $0);
+
 ffmpeg \
   -f s16le -acodec pcm_s16le -ac 2 -ar 44100 -i - \
   -f mpegts -acodec libmp3lame -ab "$1k" -ar 32000 -flags +loop+mv4 -cmp 256 \
@@ -7,4 +9,4 @@ ffmpeg \
   -me_range 16 -keyint_min 25 -sc_threshold 40 -i_qfactor 0.71 -bt "$1k" \
   -maxrate "$1k" -bufsize "$1k" -qcomp 0.6 -qmin 10 -qmax 51 -qdiff 4 \
   -level 30 -g 45 -async 2 - 2>/dev/null | \
-~/segmenter/segmenter - 10 "$1k" "$1k.m3u8" "" 5
+"$LOC/segmenter/segmenter" - 10 "$1k" "$1k.m3u8" "" 5
